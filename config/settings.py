4cb92375632fcb2f0bc3b5f0f426c944cdeb90a4
Vitor Campos <vitor.campos@spassu.com.br>
2026-04-13 09:53:21 -0300

    chore(146275): Ajuste de config do jwt

--- config/settings.py
+++ config/settings.py
@@ -147,9 +147,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [        
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
