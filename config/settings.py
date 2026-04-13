075754b1ee488879c8317c419ac480e7ab8266e0
Vitor Campos <vitor.campos@spassu.com.br>
2026-04-13 10:36:08 -0300

    chore(146275): Remoção da obrigatoriedade de passar o token de autenticação

--- config/settings.py
+++ config/settings.py
@@ -150,7 +150,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
